23671b2f32c1687547c25bb949d432fce9491b63
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-21 00:52:44 +0530

    Add files via upload
    Injecting the Collection types data and fetching the same in main class

---
+++ TestMain.java
@@ -0,0 +1,29 @@
+package com.springcore.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collection/collectionconfig.xml");
+		
+	        
+		Emp emp1 =	(Emp) context.getBean("emp1");
+		
+		
+		System.out.println("<<<   Employee Details   >>> ");
+		
+		
+		System.out.println(emp1.getName());
+		System.out.println(emp1.getPhones());
+		System.out.println(emp1.getAddresses());
+		System.out.println(emp1.getCourses());
+	
+	
+
+	}
+
+}
